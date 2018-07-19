9b01de9dcc298307be80141aadb6a3aaf7abd09a
k4w411code <41393168+k4w411code@users.noreply.github.com>
2018-07-19 09:47:17 -0400

    Create actions.js

---
+++ actions.js
@@ -0,0 +1,3 @@
+function clicker(){
+  alert("Welcome fellow foodies! Enjoy the summary of my foodie life and the GIFs that every foodie can relate to! BTW if you're not a foodie, relax....one day you will convert to FOODIEism. LOL");
+}
